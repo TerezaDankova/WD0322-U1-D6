877b71b750536c54e557ccf69e556f4158081dd6
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-07-04 15:45:32 +0200

    homework index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+let myName = "Tereza";
+let surname = "Dankova";
+let myFullName = myName +" "+ surname;
+
+console.log("\n");
+console.log("Hello, my name is", myFullName);
+console.log("\n");
+
+let myAge = 26
+let currentYear = 2022
+
+let myBirthYear = 2022 - 26
+
+console.log("And I was born in this year:", myBirthYear)
+console.log("\n");
